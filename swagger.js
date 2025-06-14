c2c16304a07c8d4ee76eb1984028f01ed9545580
Andrea Hubacek <74748748+anhubacek@users.noreply.github.com>
2025-06-14 20:39:46 -0300

    fix

--- swagger.js
+++ swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:4000',
+        url: process.env.DOMAIN,
         description: 'Servidor local de desarrollo'
       }
     ]
