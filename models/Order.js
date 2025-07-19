dd24ad6289fc01eda06aa498c549dc907dbf05a5
Andrea Hubacek <74748748+anhubacek@users.noreply.github.com>
2025-07-19 17:45:36 -0300

    order schema

--- models/Order.js
+++ models/Order.js
@@ -11,20 +11,11 @@ const CartItemSchema = new mongoose.Schema({
 });
 
 const OrderSchema = new mongoose.Schema({
-  orderId: {
-    type: String,
-    required: true,
-    unique: true
-  },
   products: [CartItemSchema],
   total: {
     type: Number,
     required: true
   },
-  timestamp: {
-    type: Number,
-    required: true
-  }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Order', OrderSchema);
